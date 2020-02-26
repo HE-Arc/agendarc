231dc4ee5f533ceef06169cb71d208aef739d1d4
daviddasilva1 <david.dasilva@he-arc.ch>
2020-02-26 13:58:39 +0100

    add blank project

---
+++ agendarc/src/main/java/com/hearc/agendarc/AgendarcApplication.java
@@ -0,0 +1,13 @@
+package com.hearc.agendarc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendarcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendarcApplication.class, args);
+	}
+
+}
---
+++ agendarc/src/test/java/com/hearc/agendarc/AgendarcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hearc.agendarc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendarcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
