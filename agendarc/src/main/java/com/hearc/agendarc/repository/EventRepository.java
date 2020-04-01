a60475e9e45a40df285bd0df9bbfe5c4c66d378b
Jeannet Nino <nino.jeannet@he-arc.ch>
2020-04-01 14:35:00 +0200

    add calendar list and calendar details page

--- agendarc/src/main/java/com/hearc/agendarc/repository/EventRepository.java
+++ agendarc/src/main/java/com/hearc/agendarc/repository/EventRepository.java
@@ -1,8 +1,13 @@
 package com.hearc.agendarc.repository;
 
+import java.util.List;
+
+import com.hearc.agendarc.model.Calendar;
 import com.hearc.agendarc.model.Event;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface EventRepository extends CrudRepository<Event, Long>{
+
+    List<Event> findByCalendar(Calendar calendar);
 }
