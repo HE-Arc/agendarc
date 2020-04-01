eccb55af52cae3957ff9d5a4c9336369929c78e6
daviddasilva1 <david.dasilva@he-arc.ch>
2020-04-01 14:02:11 +0200

    add calendar in profile page

--- agendarc/src/main/java/com/hearc/agendarc/UserController.java
+++ agendarc/src/main/java/com/hearc/agendarc/UserController.java
@@ -1,6 +1,7 @@
 package com.hearc.agendarc;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hearc.agendarc.model.Calendar;
+import com.hearc.agendarc.model.User;
+import com.hearc.agendarc.repository.CalendarRepository;
 import com.hearc.agendarc.repository.UserRepository;
 
 
@@ -18,14 +22,23 @@ public class UserController {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	CalendarRepository calendarRepository;
 
 	
 	
     @RequestMapping("/profile")
     public String profile(Model model,Principal principal)
     {
-		model.addAttribute("userConnected", userRepository.findByUsername(principal.getName()));
+		User user = userRepository.findByUsername(principal.getName());
 
+		model.addAttribute("userConnected", user);
+		
+		List<Calendar> calendars = calendarRepository.findByOwner(user);
+		model.addAttribute("number", calendars.size());
+		model.addAttribute("calendars", calendars);
+		
     	return "profile";
     }
 
